d2d255465e8124da04e964226212850b990fb010
Dan Tamas <hi@dan-tamas.me>
2016-05-04 12:25:20 +0200

    Don't cross the bridge on every translation Removed unused old file

--- i18n.android.js
+++ i18n.android.js
@@ -3,18 +3,16 @@ var application = require("application");
 var format = require('format');
 var utils = require('utils/utils');
 
+var context = utils.ad.getApplicationContext();
+var packageName = context.getPackageName();
+var resources = context.getResources();
 
 var L = function() {
-	if (arguments.length) {
-		var context = utils.ad.getApplicationContext();
-		var res = context.getResources();
-		if (res) {
-			var resID = res.getIdentifier(arguments[0], "string", context.getPackageName());
-			arguments[0] = res.getString(resID);
-			return format.apply(this, arguments);
-		}
+	if (resources && arguments.length) {
+		var resID = resources.getIdentifier(arguments[0], "string", packageName);
+		arguments[0] = resources.getString(resID);
+		return format.apply(this, arguments);
 	}
-
 };
 
 application.resources.L = L;
