da9c15c32a396c0d4284cc888a14beeaacb72e62
Dan Tamas <hi@dan-tamas.me>
2016-05-27 16:03:37 +0300

    implementation for ios

--- demo/app/app.js
+++ demo/app/app.js
@@ -2,4 +2,12 @@ require('globals');
 require('nativescript-i18n');
 
 var application = require("application");
+
+console.error('==============');
+console.error(NSBundle.mainBundle().localizedStringForKeyValueTable('hello','deff', null));
+console.error(L('hello','moo'));
+
+
 application.start({ moduleName: "main-page" });
+
+
---
+++ i18n.ios.js
@@ -0,0 +1,13 @@
+require('globals');
+var application = require("application");
+var format = require('format');
+
+var bundle = NSBundle.mainBundle();
+
+var L = function() {
+	arguments[0] = bundle.localizedStringForKeyValueTable(arguments[0],'', null);
+	return format.apply(this, arguments);
+};
+
+application.resources.L = L;
+global.L = L;
