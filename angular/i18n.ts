f4c4813d7678b24ab6ddc8d335cac2b0cb28a630
AirMike <airmike23@gmail.com>
2016-07-27 23:58:01 +0200

    Added support for Angular

---
+++ angular/i18n.ts
@@ -0,0 +1,12 @@
+require("./../i18n");
+
+import {provide, PLATFORM_PIPES} from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'i18n'})
+export class i18n implements PipeTransform {
+    transform(value: string, more: string): string {
+        return global.L(value, more);
+    }
+}
+exports.I18N_PROVIDERS = [provide(PLATFORM_PIPES, {useValue: [i18n], multi: true})];
